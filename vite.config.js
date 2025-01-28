994031c295e6870458ed467a2a354879dd47eaa9
SrijanPatel <srijanpatel911@gmail.com>
2025-01-28 11:22:51 +0530

    chneged path of images

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base:"/ems",
+  plugins: [react(), tailwindcss()],
+  base: "/ems",  // Make sure the base is correctly set for GitHub Pages or deployment
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,  // Modify chunking behavior if needed
+      },
+    },
+  },
 })

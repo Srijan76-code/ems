4c0ccb689ca868c9c55b9584839e3a978ab71698
SrijanPatel <srijanpatel911@gmail.com>
2025-01-29 09:21:36 +0530

    rechanged path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // Export configuration
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? './ems/' : './',  // Dynamic base path
+  base: process.env.NODE_ENV === 'production' ? '/ems/' : '/',  // Dynamic base path
   build: {
     rollupOptions: {
       output: {
